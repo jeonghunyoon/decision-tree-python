926147bcd2f76a670ff3786f6a56bdcadb39b8bd
davidyoon <davidyoon@coupang.com>
2016-04-18 17:49:47 +0900

    [DecisionTree-002] split 메소드 구현

--- decisiontree.py
+++ decisiontree.py
@@ -8,13 +8,21 @@ feat_vector2 = [1, 0, 'c2']
 '''
 
 
-def calc_shannon_entropy(dataset):
+def calc_shannon_entropy(datasets):
     label_counts = dict()
-    for feat_vector in dataset:
+    for feat_vector in datasets:
         current_label = feat_vector[-1]
         label_counts[current_label] = label_counts.get(current_label, 0) + 1
     shannon_entropy = 0.0
     for key in label_counts:
-        prob = float(label_counts[key]) / len(dataset)
+        prob = float(label_counts[key]) / len(datasets)
         shannon_entropy -= prob * log(prob, 2)
     return shannon_entropy
+
+
+def split_by_feat_val(datasets, idx, val):
+    split_datasets = []
+    for feat_vector in datasets:
+        if feat_vector[idx] is val:
+            split_datasets.append(feat_vector)
+    return split_datasets
